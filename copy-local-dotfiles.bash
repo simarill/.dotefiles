611558224e6a22d78d234b9137b5f551cc5c3645
Jeannine Buchmann <jeannine.buchmann@siemens.com>
2019-08-20 12:38:50 +0200

    feat: add custom regolith i3 config
    see https://www.omgubuntu.co.uk/2019/06/install-regolith-linux-i3-gaps-ubuntu

--- copy-local-dotfiles.bash
+++ copy-local-dotfiles.bash
@@ -1,6 +1,7 @@
 # copy .config/nvim/init.vim
 cp ~/.config/nvim/init.vim ~/Documents/dotfiles/.config/nvim/init.vim
 cp ~/.config/i3/* ~/Documents/dotfiles/.config/i3/
+cp ~/.config/i3-regolith/* ~/Documents/dotfiles/.config/i3-regolith/
 cp ~/.bash_aliases ~/Documents/dotfiles/.bash_aliases
 cp ~/.tmux.conf ~/Documents/dotfiles/.tmux.conf
 
